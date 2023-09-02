c0dbf47b02cef6f8f22e12a55fe63eced61baaa4
Muhammed Mamunur Rashid <96053862+mamunurrashid1010@users.noreply.github.com>
2023-09-02 14:06:33 +0600

    Post Controller index method implement and route add

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $data = Post::all();
+        return Inertia::render('posts', ['data' => $data]);
+    }
 }
